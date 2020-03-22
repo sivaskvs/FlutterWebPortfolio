4746af9a954978552a0d0c452b7eab9d2ebb8899
sivaskvs <sivasubramanian96@gmail.com>
2020-03-22 23:48:16 +0000

    removed mongodb dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,13 @@ import 'package:sivasubramanian/pages/home_page.dart';
 import 'package:sivasubramanian/widgets/theme_inherited_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase/firebase.dart'  as fb;
-import 'package:mongo_dart/mongo_dart.dart';
 
-// import 'package:dart_mongo/dart_mongo.dart' as dart_mongo;
 
 
 // import '../../../development/flutter/.pub-cache/hosted/pub.dartlang.org/firebase-7.1.0/lib/firebase.dart';
 import 'config/themes.dart';
 
 void main() {
-  
-
-
 
   if (fb.apps.isEmpty) { 
 
